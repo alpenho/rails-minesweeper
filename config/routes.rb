fcedee96801bfbcc49ddb5db1dbbb2e00ca0220b
Muhammad Alpen Saputra Halim <alpensaputra@outlook.com>
2023-10-17 23:32:05 +0700

    Make the root url to go to the Create Board Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :tiles
   resources :boards
+
+  root 'boards#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
