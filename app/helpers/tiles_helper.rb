53bcf79823af772454b0b27f6b53d9d63ebce02a
Muhammad Alpen Saputra Halim <alpensaputra@outlook.com>
2023-10-18 17:27:26 +0700

    Move the logic for calculating surrounding mines to tile models

--- app/helpers/tiles_helper.rb
+++ app/helpers/tiles_helper.rb
@@ -2,8 +2,7 @@ module TilesHelper
   def check(tile)
     ActiveRecord::Base.transaction do
       if !tile.mine
-        total_surrounding_mines = calculate_surrounding_mines(tile)
-        tile.total_surrounding_mines = total_surrounding_mines
+        tile.total_surrounding_mines = tile.calculate_surrounding_mines
       end
 
       tile.revealed = true
@@ -12,21 +11,5 @@ module TilesHelper
 
     tile.errors.add(:base, "You lose! You're clicking a bomb!") if tile.mine
     tile.errors.blank?
-  end
-
-  # algorithm to set `total_surrounding_mines`
-  def calculate_surrounding_mines(tile)
-    total_surrounding_mines = 0
-    board_id = tile.board_id
-
-    x_arr = (tile.x_pos - 1..tile.x_pos + 1).to_a
-    y_arr = (tile.y_pos - 1..tile.y_pos + 1).to_a
-
-    tiles = Tile.where(x_pos: x_arr, y_pos: y_arr, board_id: tile.board_id)
-    tiles.each do |tile_to_check|
-      total_surrounding_mines += 1 if tile_to_check.mine
-    end
-
-    total_surrounding_mines
   end
 end
--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,3 +1,18 @@
 class Tile < ApplicationRecord
   belongs_to :board
+
+  # algorithm to set `total_surrounding_mines`
+  def calculate_surrounding_mines
+    total_surrounding_mines = 0
+
+    x_arr = (x_pos - 1..x_pos + 1).to_a
+    y_arr = (y_pos - 1..y_pos + 1).to_a
+
+    tiles = Tile.where(x_pos: x_arr, y_pos: y_arr, board_id: board_id)
+    tiles.each do |tile_to_check|
+      total_surrounding_mines += 1 if tile_to_check.mine
+    end
+
+    total_surrounding_mines
+  end
 end
